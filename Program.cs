d3ae683b9b5a0bf1f0786357611809065be910d0
Diego Boschetti <dlboschetti@gmail.com>
2020-08-02 23:36:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercicio_logica_programacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rejuste salarial:
+
+            //Faça um algoritmo que calcule o reajuste de um salário. 
+            //O usuário deverá informar o salário e o percentual de reajuste para a realização do cálculo.
+
+            Console.WriteLine("Favor informar o salário que a ser ajustado (R$):");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Favor informar o percentual do reajuste (%):");
+            double percReajuste = Convert.ToDouble(Console.ReadLine());
+            if (percReajuste >0)
+            {
+                double salAjustado = salario * (1 + (percReajuste / 100));
+                Console.WriteLine($"Valor do novo salário (R$):{salAjustado}");
+            }
+            else
+                Console.WriteLine("Informação incorreta!");
+
+        }
+    }
+}
